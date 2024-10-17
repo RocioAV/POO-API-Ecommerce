4b27cbc339223682effdf1837402863d213a7b5a
rhovelazquez <rocyvelazquez2021@gmail.com>
2024-10-17 20:37:13 -0300

    First commit

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/Tp8PooApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.poo.tp8poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp8PooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp8PooApplication.class, args);
+    }
+
+}
