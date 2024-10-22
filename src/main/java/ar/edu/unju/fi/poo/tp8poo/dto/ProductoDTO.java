d8b12de534b672e1a5741cdaa6584ff4997aeaec
RocioAV <44351449@fi.unju.edu.ar>
2024-10-22 00:20:04 -0300

    Creacion producto y productoDTO

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/dto/ProductoDTO.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.poo.tp8poo.dto;
+
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductoDTO {
+    private Long id;
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer cantidad;
+    private String imagen;
+    private String estado;
+    private ProveedorDTO proveedor;
+}
