d88e6db52216a930f184f74efdfa9bd839a95839
RocioAV <44351449@fi.unju.edu.ar>
2024-10-22 14:24:51 -0300

    Creacion de nuevas excepciones para validar email y celular

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/exceptions/EmailDuplicadoException.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.poo.tp8poo.exceptions;
+
+public class EmailDuplicadoException extends RuntimeException {
+    public EmailDuplicadoException(String message) {
+        super(message);
+    }
+}
