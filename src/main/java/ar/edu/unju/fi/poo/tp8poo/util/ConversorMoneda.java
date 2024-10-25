ccd546c30ed3650396f1c387ceedc95e19be2e2f
Fabricio <fabri98201098@gmail.com>
2024-10-24 23:14:28 -0300

    Desarrollo del conversor de moneda

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/util/ConversorMoneda.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.fi.poo.tp8poo.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+
+public class ConversorMoneda {
+	
+	
+	private String consumoAPI() throws IOException {
+			int cp;
+			URL url= new URL("https://dolarapi.com/v1/dolares/oficial");
+			HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accept", "application/json");
+			if(connection.getResponseCode()!=200) {
+				throw new RuntimeException("Error con el código HTTP "+connection.getResponseCode());
+			}
+			BufferedReader br= new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuilder str=new StringBuilder();
+			while((cp=br.read())!=-1) {
+				str.append((char)cp);
+			}
+			return str.toString();
+	}
+	
+	public double convertirPrecio(double precioActual) throws IOException {
+		double precioVenta;
+		String api=consumoAPI();
+		JSONObject json= new JSONObject(api);
+		precioVenta=json.getDouble("venta")*precioActual;
+		return precioVenta;
+	}
+	
+	
+}
