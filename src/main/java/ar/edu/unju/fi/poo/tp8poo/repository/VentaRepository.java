2039ac5b37d6822a6957742faa5de97b0a8d24a7
RocioAV <44351449@fi.unju.edu.ar>
2024-10-26 23:26:01 -0300

    add: Se creo el VentaRepository

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/repository/VentaRepository.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.poo.tp8poo.repository;
+
+import ar.edu.unju.fi.poo.tp8poo.entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
