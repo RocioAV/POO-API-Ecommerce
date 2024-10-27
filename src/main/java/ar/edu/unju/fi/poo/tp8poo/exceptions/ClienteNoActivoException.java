8ff3635cfa642e7b25318310741c2829a7a4b150
RocioAV <44351449@fi.unju.edu.ar>
2024-10-27 16:14:54 -0300

    add: creacion de exception para Service de venta

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/exceptions/ClienteNoActivoException.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.poo.tp8poo.exceptions;
+
+public class ClienteNoActivoException extends RuntimeException {
+    public ClienteNoActivoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/exceptions/ProductoNoDIsponibleException.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.poo.tp8poo.exceptions;
+
+public class ProductoNoDIsponibleException extends RuntimeException {
+    public ProductoNoDIsponibleException(String message) {
+        super(message);
+    }
+}
