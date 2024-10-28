26455c440cd4dd13c1b65cd5443a7fc2d83f25ec
RocioAV <44351449@fi.unju.edu.ar>
2024-10-28 02:08:03 -0300

    add: Creacion de nuevas excepciones

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/exceptions/DebitoVencidaException.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.poo.tp8poo.exceptions;
+
+public class DebitoVencidaException extends RuntimeException {
+    public DebitoVencidaException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ar/edu/unju/fi/poo/tp8poo/exceptions/ProductoNoDIsponibleException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unju.fi.poo.tp8poo.exceptions;
-
-public class ProductoNoDIsponibleException extends RuntimeException {
-    public ProductoNoDIsponibleException(String message) {
-        super(message);
-    }
-}
