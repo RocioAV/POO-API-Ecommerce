548a1342fea0503307a222b37c19364804b702a4
RocioAV <44351449@fi.unju.edu.ar>
2024-10-31 17:48:51 -0300

    eliminacion de exceptions sin uso

--- src/main/java/ar/edu/unju/fi/poo/tp8poo/exceptions/ProductoSinStockException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unju.fi.poo.tp8poo.exceptions;
-
-public class ProductoSinStockException extends RuntimeException {
-    public ProductoSinStockException(String message) {
-        super(message);
-    }
-}
