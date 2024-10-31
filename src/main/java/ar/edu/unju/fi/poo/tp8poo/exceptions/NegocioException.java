4c32f0f1d694f7f9a58c46a421ec138762099428
Fabricio <fabri98201098@gmail.com>
2024-10-31 15:55:02 -0300

    Creacion de una sola clase Exception

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/exceptions/NegocioException.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.poo.tp8poo.exceptions;
+
+public class NegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public NegocioException(String message) {
+		super(message);
+	}
+}
