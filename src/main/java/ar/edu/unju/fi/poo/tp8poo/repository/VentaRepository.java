ba56237755654f0a719113857eba50c7b4e64178
RocioAV <44351449@fi.unju.edu.ar>
2024-11-03 03:40:56 -0300

    add: creacion de FiltroVentaDTO y creacion de queryMethod en el repository de ventas

--- src/main/java/ar/edu/unju/fi/poo/tp8poo/repository/VentaRepository.java
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/repository/VentaRepository.java
@@ -4,6 +4,12 @@ import ar.edu.unju.fi.poo.tp8poo.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Long> {
+    List<Venta> findByClienteNombreContaining(String nombre); // Filtrar por nombre del cliente
+    List<Venta> findByClienteId(Long id);               // Filtrar por DNI del cliente
+    List<Venta> findByFechaYHoraBetween(LocalDateTime fechaDesde, LocalDateTime fechaHasta); // Filtrar por rango de fechas
 }
