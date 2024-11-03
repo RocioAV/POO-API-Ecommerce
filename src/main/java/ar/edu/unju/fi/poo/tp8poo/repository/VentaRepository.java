920123fc9c546a318292b8b2e20952298036fadd
RocioAV <44351449@fi.unju.edu.ar>
2024-11-03 20:52:32 -0300

    fix: se modifico a solo una query method en VentaRepository

--- src/main/java/ar/edu/unju/fi/poo/tp8poo/repository/VentaRepository.java
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/repository/VentaRepository.java
@@ -2,6 +2,8 @@ package ar.edu.unju.fi.poo.tp8poo.repository;
 
 import ar.edu.unju.fi.poo.tp8poo.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -9,7 +11,13 @@ import java.util.List;
 
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Long> {
-    List<Venta> findByClienteNombreContaining(String nombre); // Filtrar por nombre del cliente
-    List<Venta> findByClienteId(Long id);               // Filtrar por DNI del cliente
-    List<Venta> findByFechaYHoraBetween(LocalDateTime fechaDesde, LocalDateTime fechaHasta); // Filtrar por rango de fechas
+    @Query("SELECT v FROM Venta v WHERE " +
+            "(:nombreCliente IS NULL OR v.cliente.nombre LIKE %:nombreCliente%) AND " +
+            "(:idCliente IS NULL OR v.cliente.id = :idCliente) AND " +
+            "(:fechaDesde IS NULL OR v.fechaYHora >= :fechaDesde) AND " +
+            "(:fechaHasta IS NULL OR v.fechaYHora <= :fechaHasta)")
+    List<Venta> filtrarVentas(@Param("nombreCliente") String nombreCliente,
+                              @Param("idCliente") Long idCliente,
+                              @Param("fechaDesde") LocalDateTime fechaDesde,
+                              @Param("fechaHasta") LocalDateTime fechaHasta);
 }
