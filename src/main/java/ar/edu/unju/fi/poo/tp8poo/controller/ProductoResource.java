14ef1dbfa87d122aa008f3a6d1c8822b5bf0bc9a
RocioAV <44351449@fi.unju.edu.ar>
2024-11-02 23:29:16 -0300

    Se agrega trazas y respectivos trycatch para las distintas peticiones, cambio de nombre a ProductoResource

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/controller/ProductoResource.java
@@ -0,0 +1,149 @@
+package ar.edu.unju.fi.poo.tp8poo.controller;
+
+import ar.edu.unju.fi.poo.tp8poo.dto.ProductoDTO;
+import ar.edu.unju.fi.poo.tp8poo.exceptions.NegocioException;
+import ar.edu.unju.fi.poo.tp8poo.service.ProductoService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/producto")
+@Slf4j
+public class ProductoResource {
+    private final ProductoService productoService;
+
+    public ProductoResource(ProductoService productoService) {this.productoService = productoService;}
+
+    @GetMapping("/list")
+    public ResponseEntity<?> getAllProductos() {
+        log.info("/api/v1/producto/list");
+        Map<String, Object> response = new HashMap<>();
+        try {
+            List<ProductoDTO> productos = productoService.findAll();
+            if (productos.isEmpty()) {
+                log.info("No se encontraron productos");
+                return ResponseEntity.noContent().build();
+            }
+            response.put("productos", productos);
+            response.put("mensaje", "Productos obtenidos con éxito");
+            return ResponseEntity.ok(response);
+        }catch (DataAccessException e) {
+            log.error("Error de acceso a la base de datos al obtener el producto por ID", e);
+            response.put("mensaje", "Error de base de datos al obtener el producto");
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getProductoById(@PathVariable Long id) {
+        log.info("/api/v1/producto/get/{id}", id);
+        Map<String, Object> response = new HashMap<>();
+        try {
+            ProductoDTO productoDTO = productoService.findById(id);
+            if (productoDTO == null) {
+                log.warn("Producto con ID [{}] no encontrado", id);
+                return ResponseEntity.notFound().build();
+            }
+            response.put("producto", productoDTO);
+            response.put("mensaje", "Producto obtenido con éxito");
+            return ResponseEntity.ok(response);
+        }catch (EntityNotFoundException e){
+            log.error("No se encontro el producto con ID [{}]", id);
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }catch (DataAccessException e) {
+            log.error("Error de acceso a la base de datos al obtener el producto por ID", e);
+            response.put("mensaje", "Error de base de datos al obtener el producto");
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createProducto(@RequestBody ProductoDTO productoDTO) {
+        log.info("/api/v1/producto/create");
+        Map<String, Object> response = new HashMap<>();
+        try{
+            ProductoDTO createdProducto = productoService.createProducto(productoDTO);
+            response.put("producto", createdProducto);
+            response.put("mensaje", "Producto creado con éxito");
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (NegocioException e) {
+            log.warn("Error al crear el producto: {}", e.getMessage());
+            response.put("mensaje", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }catch (DataAccessException e) {
+            log.error("Error de acceso a la base de datos al crear el producto", e);
+            response.put("mensaje", "Error de base de datos al crear el producto");
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateProducto(@PathVariable Long id, @RequestBody ProductoDTO productoDTO) {
+        log.info("/api/v1/producto/update/{id}", id);
+        Map<String, Object> response = new HashMap<>();
+        try{
+            ProductoDTO updatedProducto = productoService.editProducto(id, productoDTO);
+            if (updatedProducto == null) {
+                log.warn("Producto con ID [{}] no encontrado para actualizar", id);
+                return ResponseEntity.notFound().build();
+            }
+            response.put("producto", updatedProducto);
+            response.put("mensaje", "Producto actualizado con éxito");
+            return ResponseEntity.ok(response);
+        }catch (EntityNotFoundException e){
+            log.error("No se encontro el producto con ID [{}] para editar", id);
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }catch (DataAccessException e) {
+            log.error("Error de acceso a la base de datos al actualizar el producto", e);
+            response.put("mensaje", "Error de base de datos al actualizar el producto");
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProducto(@PathVariable Long id) {
+        log.info("/api/v1/producto/delete/{id}", id);
+        Map<String, Object> response = new HashMap<>();
+        try{
+            ProductoDTO producto = productoService.deleteProductoLogico(id);
+            if (producto == null) {
+                log.warn("Producto con ID [{}] no encontrado para eliminar", id);
+                return ResponseEntity.notFound().build();
+            }
+            response.put("producto", producto);
+            response.put("mensaje", "Producto eliminado con éxito");
+            return ResponseEntity.ok(response);
+        }catch (EntityNotFoundException e){
+            log.error("No se encontro el producto con ID [{}] para eliminar", id);
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }catch (DataAccessException e) {
+            log.error("Error de acceso a la base de datos al eliminar el producto", e);
+            response.put("mensaje", "Error de base de datos al eliminar el producto");
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+
+    }
+
+
+
+}
