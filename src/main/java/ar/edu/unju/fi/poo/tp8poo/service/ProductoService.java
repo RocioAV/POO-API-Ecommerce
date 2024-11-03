90ccb2459c185f3a566d8ec7bb828ad70f6e17ce
RocioAV <44351449@fi.unju.edu.ar>
2024-11-02 21:30:21 -0300

    creacion de productoController modificacion al service de producto

--- src/main/java/ar/edu/unju/fi/poo/tp8poo/service/ProductoService.java
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/service/ProductoService.java
@@ -103,9 +103,10 @@ public class ProductoService {
      * Realiza un borrado lógico de un producto, cambiando su estado a false.
      *
      * @param id El ID del producto a eliminar lógicamente.
+     * @return
      * @throws EntityNotFoundException Si no se encuentra un producto con el ID proporcionado.
      */
-    public void deleteProductoLogico(Long id) {
+    public ProductoDTO deleteProductoLogico(Long id) {
         log.info("Realizando borrado lógico de producto con ID: {}", id);
         Producto producto = productoRepository.findById(id)
                 .orElseThrow(() -> {
@@ -113,8 +114,10 @@ public class ProductoService {
                     return new EntityNotFoundException("Producto no encontrado");
                 });
         producto.setEstado(EstadoProducto.NO_DISPONIBLE.getEstado());
-        productoRepository.save(producto);
         log.info("Borrado lógico realizado con éxito para el producto con ID: {}", id);
+        return productoMapper.toProductoDTO(productoRepository.save(producto));
+
+
     }
 
     /**
@@ -143,7 +146,7 @@ public class ProductoService {
     public List<ProductoDTO> findAll() {
         log.info("Obteniendo todos los productos disponibles");
 
-        List<ProductoDTO> productosDTO = productoMapper.toProductoDTOList(productoRepository.findByEstado(EstadoProducto.DISPONIBLE.getEstado()));
+        List<ProductoDTO> productosDTO = productoMapper.toProductoDTOList(productoRepository.findAll());
         log.info("Productos encontrados: {}", productosDTO.size());
         return productosDTO;
 
