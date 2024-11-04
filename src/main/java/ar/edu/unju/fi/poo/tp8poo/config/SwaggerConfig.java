9da8a7d4a036875e2a1d0ac5c6fc5a9918e47e6b
Fabricio Bazan <fabri98201098@gmail.com>
2024-11-04 04:13:43 -0300

    Agregado de configuracion de Swagger

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package ar.edu.unju.fi.poo.tp8poo.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API NEGOCIO",
+                description="Nuestra aplicacion provee gestion del negocio, ya sea de cliente, producto, proveedor y sus ventas",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Velazques, Rocio. Valle, Alejandro. Bazan, Fabricio",
+                        email = "44351449@fi.unju.edu.ar, alejandrovalle1904@gmail.com, 41300614@fi.unju.edu.ar"
+                ),
+                license = @License(
+                        name = "Standard software Use Licence for Grupo2"
+                )
+
+        ),
+        servers = {
+                @Server(
+                        description = "DEV SERVER",
+                        url = "http://localhost:9000"
+                )
+        }
+)
+
+
+public class SwaggerConfig {}
+
+
+
