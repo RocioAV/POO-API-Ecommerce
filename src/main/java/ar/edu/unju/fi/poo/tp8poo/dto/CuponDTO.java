7050dcc7d8ca01eb66b54fe003c047d279dcc130
Fabricio Bazan <fabri98201098@gmail.com>
2024-11-04 04:20:16 -0300

    Documentacion de CuponDTO

--- src/main/java/ar/edu/unju/fi/poo/tp8poo/dto/CuponDTO.java
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/dto/CuponDTO.java
@@ -1,6 +1,7 @@
 package ar.edu.unju.fi.poo.tp8poo.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CuponDTO {
 
+    @Schema(description = "ID del cupón", example = "1")
     private Long id;
 
+    @Schema(description = "Fecha de expiración del cupón", example = "2024-12-31")
     private String fechaExpiracion;
 
+    @Schema(description = "Porcentaje de descuento del cupón", example = "10.0")
     private double porcentajeDescuento;
 }
