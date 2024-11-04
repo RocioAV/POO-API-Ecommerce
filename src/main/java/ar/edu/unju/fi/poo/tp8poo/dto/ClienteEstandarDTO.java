4ad38e563e8e64ab8cad20d7494aaf984ae80661
Fabricio Bazan <fabri98201098@gmail.com>
2024-11-04 04:20:43 -0300

    Documentacion de ClienteEstandarDTO

--- src/main/java/ar/edu/unju/fi/poo/tp8poo/dto/ClienteEstandarDTO.java
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/dto/ClienteEstandarDTO.java
@@ -1,8 +1,10 @@
 package ar.edu.unju.fi.poo.tp8poo.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @ToString
@@ -11,12 +13,10 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class ClienteEstandarDTO extends ClienteDTO implements Serializable{
-	/**
-	 * 
-	 */
+public class ClienteEstandarDTO extends ClienteDTO implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
+	@Schema(description = "Cupon de descuento asignado al cliente estándar")
 	private CuponDTO cupon;
-
 }
