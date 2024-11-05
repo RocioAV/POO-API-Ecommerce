419e8d1be58e6da641b14b8a54254d19bb0f6d9f
leoalevalle2014 <leoalevalle2014@gmail.com>
2024-11-04 22:32:42 -0300

    fix: Documentacion de ProveedorDTO

--- src/main/java/ar/edu/unju/fi/poo/tp8poo/dto/ProveedorDTO.java
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/dto/ProveedorDTO.java
@@ -1,5 +1,6 @@
 package ar.edu.unju.fi.poo.tp8poo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
@@ -12,14 +13,19 @@ import lombok.NoArgsConstructor;
 @Component
 public class ProveedorDTO {
 
+	@Schema(description = "Identificador único del proveedor")
     private Long id;
 
+    @Schema(description = "Nombre del proveedor", example = "Proveedor S.A.")
     private String nombre;
 
+    @Schema(description = "Correo electrónico del proveedor", example = "proveedor@email.com")
     private String email;
 
+    @Schema(description = "Número de teléfono del proveedor", example = "+541234567890")
     private String telefono;
 
+    @Schema(description = "Estado del proveedor, activo o inactivo")
     private boolean estado;
 
 }
