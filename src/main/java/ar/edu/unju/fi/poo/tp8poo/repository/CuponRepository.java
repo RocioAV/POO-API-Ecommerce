7be3591eb8210cd8858c36078c93c0d181769aea
Fabricio Bazan <fabri98201098@gmail.com>
2024-11-13 14:04:17 -0300

    Se ha añadido un repository de cupon.

---
+++ src/main/java/ar/edu/unju/fi/poo/tp8poo/repository/CuponRepository.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.poo.tp8poo.repository;
+
+import ar.edu.unju.fi.poo.tp8poo.entity.Cupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuponRepository extends JpaRepository<Cupon,Long> {
+}
